be8e61f7a6facc4cad791f4d1f4735be7ad6496e
Máté Cserháti <mate.cserhati03@gmail.com>
2024-04-23 22:12:39 +0200

    Dark mode and backend code is prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media',
   content: ["./public/*.html", "./public/js/*.js","./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
